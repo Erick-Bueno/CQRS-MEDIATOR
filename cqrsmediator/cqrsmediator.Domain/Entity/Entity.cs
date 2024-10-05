0c44ebe5944c66514ee59cfc83cdb4b268558982
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2024-10-05 12:45:32 -0300

    feat: add base Entity

--- cqrsmediator/cqrsmediator.Domain/Entity/Entity.cs
+++ cqrsmediator/cqrsmediator.Domain/Entity/Entity.cs
@@ -3,10 +3,6 @@ namespace cqrsmediator.Domain.Entity
     public abstract class Entity
     {
         public Guid Id { get; protected set; }
-        protected Entity()
-        {
-            Id = Guid.NewGuid();
-        }
     }
 
 }
