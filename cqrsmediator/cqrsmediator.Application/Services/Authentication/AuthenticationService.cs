3e43a9a1d0515b11165d615da719b6d4dbcd5f9b
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2024-10-09 06:32:11 -0300

    feat: implementar interface referente na classe authentication service

---
+++ cqrsmediator/cqrsmediator.Application/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,18 @@
+using cqrsmediator.Application.Request;
+using cqrsmediator.Application.Response;
+
+namespace cqrsmediator.Application.Services.Authentication
+{
+    internal class AuthenticationService : IAuthenticationService
+    {
+        public Task<LoginResponse> Login(LoginRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RegisterResponse> Register(RegisterRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
