7de3d94a484d7c74e66ddceb18f2b5a1e3a38bab
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2024-10-09 06:35:13 -0300

    feat: injetar autentication service em authentication controller

--- cqrsmediator/cqrsmediator.Api/Controllers/AuthenticationController.cs
+++ cqrsmediator/cqrsmediator.Api/Controllers/AuthenticationController.cs
@@ -1,4 +1,5 @@
 using cqrsmediator.Application.Request;
+using cqrsmediator.Application.Services.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
 namespace cqrsmediator.Api.Controllers
@@ -7,15 +8,19 @@ namespace cqrsmediator.Api.Controllers
     [Route("auth")]
     public class AuthenticationController : ControllerBase
     {
+        private readonly IAuthenticationService _authenticationService;
+        public AuthenticationController(IAuthenticationService authenticationService){
+            _authenticationService = authenticationService;
+        }
         [HttpPost("register")]
         public async Task<IActionResult> Register(RegisterRequest request)
         {
-
+            return Ok(request);
         }
         [HttpPost("login")]
         public async Task<IActionResult> Login(LoginRequest request)
         {
-
+            return Ok(request);
         }
     }
 }
