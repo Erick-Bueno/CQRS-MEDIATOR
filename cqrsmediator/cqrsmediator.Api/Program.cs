7a249fed2cc7a1a48d0e78418d12063c3ddce08a
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2024-10-09 07:30:55 -0300

    feat: adicionar referencia a classe de depencias da camada apllication no arquivo program

--- cqrsmediator/cqrsmediator.Api/Program.cs
+++ cqrsmediator/cqrsmediator.Api/Program.cs
@@ -1,3 +1,4 @@
+using cqrsmediator.Application;
 using cqrsmediator.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
 builder.Services.AddInfrastructure();
+builder.Services.AddApplication();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
